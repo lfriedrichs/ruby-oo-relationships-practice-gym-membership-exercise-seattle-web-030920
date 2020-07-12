1b3ba9bb4f16ea09ffb312e45bf617e04fadc5f7
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-12 16:14:38 +0000

    Automatically backed up by Learn

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,11 @@
 class Lifter
   attr_reader :name, :lift_total
+  
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
 end
